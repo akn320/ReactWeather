7b088f36db2a739a0a9a79eb900213f049a4f67b
Audrey <audrey.kazuko@gmail.com>
2017-06-06 20:47:55 -0700

    Add Foundations and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,12 @@ var ReactDOM = require("react-dom");
 var Main = require('Main');
 var {HashRouter, Switch, Route, hashHistory, Link} = require('react-router-dom');
 
+//Load foundation
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+jQuery(document).ready(function($) {
+  $(document).foundation();
+});
+
 ReactDOM.render(
   <HashRouter>
     <Main/>
