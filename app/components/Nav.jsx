3f66880b495fc1138d7c12108bfd2858cb121125
Audrey <audrey.kazuko@gmail.com>
2017-06-06 21:01:32 -0700

    Style Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,45 @@
 var React = require('react');
 var {Link} = require('react-router-dom');
+var createReactClass = require('create-react-class');
 
-var Nav = (props) => {
-  return (
-      <div>
-        <h2>Nav component</h2>
-        <Link to='/'>Get Weather</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/examples'>Examples</Link>
+
+var Nav = createReactClass({
+  onSearch: function(e) {
+    e.preventDefault();
+    alert('Not yet wired up');
+  },
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Weather App</li>
+            <li>
+              <Link to='/'>Get Weather</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/examples'>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className='menu'>
+              <li>
+                <input type="search" placeholder='Search Weather'/>
+              </li>
+              <li>
+                <input type="submit" className='button' value='Get Weather'/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
-  );
-};
+    );
+  }
+});
 
 module.exports = Nav;
